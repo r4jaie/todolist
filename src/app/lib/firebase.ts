ec5ee3822f923472b166d7980888a3ecf244491f
r4jaie <rajaie4akbar@gmail.com>
2025-04-11 10:17:46 +0700

    Merge the folder todolist

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,14 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // isi konfigurasi sesuai dengan konfigurasi firebase kalian
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyACCeZOQy2X5zPwxu2P1AmRy4HZb1s4RbI",
+  authDomain: "to-do-list-f65d5.firebaseapp.com",
+  projectId: "to-do-list-f65d5",
+  storageBucket: "to-do-list-f65d5.firebasestorage.app",
+  messagingSenderId: "921081749662",
+  appId: "1:921081749662:web:1e4db4a3eccf551675db8f",
+  measurementId: "G-BZTTTD515S",
 };
 
 // Inisialisasi Firebase
